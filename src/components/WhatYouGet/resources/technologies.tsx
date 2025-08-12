e1278e9e31622dd6403829f2248a7a4dfcfa4f91
pacozavala <paco.zavala@houston-inc.com>
2025-08-12 10:31:52 +0300

    Update technology certification URLs and improve formatting for consistency

--- src/components/WhatYouGet/resources/technologies.tsx
+++ src/components/WhatYouGet/resources/technologies.tsx
@@ -8,12 +8,12 @@ import {
   SQLLogo,
   TailwindCss,
   Typescript,
-  GoogleCloud
+  GoogleCloud,
 } from '@/components/icons'
 
 type Technologies = {
   name: string
-  certUrl?:string
+  certUrl?: string
   imageUrl?: string
   altName?: string
   imageExtraStyle: string
@@ -21,7 +21,6 @@ type Technologies = {
   IconComponent?: React.ReactNode
   skillToolTipMsg: boolean
 }
-
 
 export const technologies: Technologies[] = [
   {
@@ -32,7 +31,7 @@ export const technologies: Technologies[] = [
     imageExtraStyle: 'object-contain',
     darkThemeTextColour: 'dark:text-blue-500',
     skillToolTipMsg: true,
-    IconComponent: <Typescript className="skill-logo object-contain" />
+    IconComponent: <Typescript className="skill-logo object-contain" />,
   },
   {
     name: 'Google Cloud',
@@ -40,7 +39,7 @@ export const technologies: Technologies[] = [
     darkThemeTextColour: 'dark:text-blue-400',
     skillToolTipMsg: false,
     IconComponent: <GoogleCloud className="skill-logo" />,
-    imageExtraStyle: ""
+    imageExtraStyle: '',
   },
   {
     name: 'Javascript',
@@ -64,30 +63,33 @@ export const technologies: Technologies[] = [
   },
   {
     name: 'Docker',
-    certUrl: 'https://res.cloudinary.com/dbn5gpgi5/image/upload/v1681117643/q26fq714x9njo4pxskps.png',
+    certUrl:
+      'https://res.cloudinary.com/dbn5gpgi5/image/upload/v1681117643/q26fq714x9njo4pxskps.png',
     altName: 'Docker Logo',
     darkThemeTextColour: 'dark:text-blue-500',
     IconComponent: <DockerLogo className="skill-logo bg-transparent" />,
     skillToolTipMsg: true,
-    imageExtraStyle: ""
+    imageExtraStyle: '',
   },
   {
     name: 'Kubernetes',
-    certUrl: '',
+    certUrl:
+      'https://res.cloudinary.com/dbn5gpgi5/image/upload/v1754983669/wi5ihb6yp62yh6wfpfve.png',
     imageUrl: 'https://img.icons8.com/color/600/000000/kubernetes.png',
     altName: 'Kubernetes Logo',
     darkThemeTextColour: 'dark:text-blue-500',
-    skillToolTipMsg: false,
-    imageExtraStyle: ""
+    skillToolTipMsg: true,
+    imageExtraStyle: '',
   },
   {
-      name: 'Redux',
-      certUrl: 'https://res.cloudinary.com/dbn5gpgi5/image/upload/v1681117638/ji8dwtahm9dom6kuzxbm.png',
-      altName: 'Javascript Logo',
-      darkThemeTextColour: 'dark:text-blue-300',
-      skillToolTipMsg: true,
-      IconComponent: <Redux className="skill-logo" />,
-      imageExtraStyle: ""
+    name: 'Redux',
+    certUrl:
+      'https://res.cloudinary.com/dbn5gpgi5/image/upload/v1681117638/ji8dwtahm9dom6kuzxbm.png',
+    altName: 'Javascript Logo',
+    darkThemeTextColour: 'dark:text-blue-300',
+    skillToolTipMsg: true,
+    IconComponent: <Redux className="skill-logo" />,
+    imageExtraStyle: '',
   },
   {
     name: 'GraphQL',
@@ -103,28 +105,29 @@ export const technologies: Technologies[] = [
     name: 'React-Native',
     certUrl:
       'https://res.cloudinary.com/dbn5gpgi5/image/upload/v1681117638/ji8dwtahm9dom6kuzxbm.png',
-    imageUrl: "https://img.icons8.com/nolan/600/react-native.png",
+    imageUrl: 'https://img.icons8.com/nolan/600/react-native.png',
     altName: 'React-Native Logo',
     imageExtraStyle: 'object-contain',
     darkThemeTextColour: 'dark:text-pink-700',
     skillToolTipMsg: true,
   },
   {
-      name: 'CSS',
-      IconComponent: <Css className="skill-logo" />,
-      altName: 'CSS Logo',
-      darkThemeTextColour: 'dark:text-blue-600',
-      skillToolTipMsg: false,
-      imageExtraStyle: ""
+    name: 'CSS',
+    IconComponent: <Css className="skill-logo" />,
+    altName: 'CSS Logo',
+    darkThemeTextColour: 'dark:text-blue-600',
+    skillToolTipMsg: false,
+    imageExtraStyle: '',
   },
   {
-      name: 'MongoDB',
-      certUrl: 'https://res.cloudinary.com/dbn5gpgi5/image/upload/v1681117638/ji8dwtahm9dom6kuzxbm.png',
-      imageUrl: 'https://img.icons8.com/color/600/000000/mongodb.png',
-      altName: 'MongoDB Logo',
-      darkThemeTextColour: 'dark:text-green-400',
-      skillToolTipMsg: true,
-      imageExtraStyle: ""
+    name: 'MongoDB',
+    certUrl:
+      'https://res.cloudinary.com/dbn5gpgi5/image/upload/v1681117638/ji8dwtahm9dom6kuzxbm.png',
+    imageUrl: 'https://img.icons8.com/color/600/000000/mongodb.png',
+    altName: 'MongoDB Logo',
+    darkThemeTextColour: 'dark:text-green-400',
+    skillToolTipMsg: true,
+    imageExtraStyle: '',
   },
   {
     name: 'Node.js',
@@ -137,56 +140,62 @@ export const technologies: Technologies[] = [
     skillToolTipMsg: true,
   },
   {
-      name: 'SQL',
-      certUrl: 'https://res-cloudinary-com/dbn5gpgi5/image/upload/v1681117640/zomoudnr2mriz6zvdetq.png',
-      altName: 'SQL Logo',
-      darkThemeTextColour: 'dark:text-blue-400',
-      IconComponent: <SQLLogo className="skill-logo" />,
-      skillToolTipMsg: true,
-      imageExtraStyle: ""
+    name: 'SQL',
+    certUrl:
+      'https://res-cloudinary-com/dbn5gpgi5/image/upload/v1681117640/zomoudnr2mriz6zvdetq.png',
+    altName: 'SQL Logo',
+    darkThemeTextColour: 'dark:text-blue-400',
+    IconComponent: <SQLLogo className="skill-logo" />,
+    skillToolTipMsg: true,
+    imageExtraStyle: '',
   },
   {
-      name: 'Tailwindcss',
-      altName: 'Tailwindcss Logo',
-      darkThemeTextColour: 'dark:text-blue-400',
-      IconComponent: <TailwindCss className="skill-logo object-contain" />,
-      skillToolTipMsg: false,
-      imageExtraStyle: ""
+    name: 'Tailwindcss',
+    altName: 'Tailwindcss Logo',
+    darkThemeTextColour: 'dark:text-blue-400',
+    IconComponent: <TailwindCss className="skill-logo object-contain" />,
+    skillToolTipMsg: false,
+    imageExtraStyle: '',
   },
   {
-      name: 'Cypress.io',
-      certUrl: 'https://res.cloudinary.com/dbn5gpgi5/image/upload/v1681117638/ji8dwtahm9dom6kuzxbm.png',
-      altName: 'Cypress.io Logo',
-      darkThemeTextColour: 'dark:text-gray-200',
-      IconComponent: <Cypress className="skill-logo" />,
-      skillToolTipMsg: true,
-      imageExtraStyle: ""
+    name: 'Cypress.io',
+    certUrl:
+      'https://res.cloudinary.com/dbn5gpgi5/image/upload/v1681117638/ji8dwtahm9dom6kuzxbm.png',
+    altName: 'Cypress.io Logo',
+    darkThemeTextColour: 'dark:text-gray-200',
+    IconComponent: <Cypress className="skill-logo" />,
+    skillToolTipMsg: true,
+    imageExtraStyle: '',
   },
   {
-      name: 'Datadog',
-      certUrl: 'https://datadog.docebosaas.com/share/v1/gamification/assigned_badge/942dde0a-211f-4e54-b9ea-fe148117eec0/shared?lang=en&t=1689281055089',
-      imageUrl: 'https://cdn5.dcbstatic.com/files/d/a/datadog_docebosaas_com/assets/badges/original/6149e0bf43bb02e0d6e207efd297a00ce5ac4443.png',
-      altName: 'Datadog Logo',
-      darkThemeTextColour: 'dark:text-green-500',
-      skillToolTipMsg: true,
-      imageExtraStyle: ""
+    name: 'Datadog',
+    certUrl:
+      'https://datadog.docebosaas.com/share/v1/gamification/assigned_badge/942dde0a-211f-4e54-b9ea-fe148117eec0/shared?lang=en&t=1689281055089',
+    imageUrl:
+      'https://cdn5.dcbstatic.com/files/d/a/datadog_docebosaas_com/assets/badges/original/6149e0bf43bb02e0d6e207efd297a00ce5ac4443.png',
+    altName: 'Datadog Logo',
+    darkThemeTextColour: 'dark:text-green-500',
+    skillToolTipMsg: true,
+    imageExtraStyle: '',
   },
   {
-      name: 'Java Programming I',
-      certUrl: 'https://res.cloudinary.com/dbn5gpgi5/image/upload/v1681118753/rtibuwlmtzmdtr35u2gw.png',
-      altName: 'Java Programming I Logo',
-      darkThemeTextColour: 'dark:text-yellow-600',
-      IconComponent: <Java className="skill-logo" />,
-      skillToolTipMsg: true,
-      imageExtraStyle: ""
+    name: 'Java Programming I',
+    certUrl:
+      'https://res.cloudinary.com/dbn5gpgi5/image/upload/v1681118753/rtibuwlmtzmdtr35u2gw.png',
+    altName: 'Java Programming I Logo',
+    darkThemeTextColour: 'dark:text-yellow-600',
+    IconComponent: <Java className="skill-logo" />,
+    skillToolTipMsg: true,
+    imageExtraStyle: '',
   },
   {
-      name: 'Java Programming II',
-      certUrl: 'https://res.cloudinary.com/dbn5gpgi5/image/upload/v1681118770/v7jfhsjvhla0pyw1p1dr.png',
-      altName: 'Java Programming II Logo',
-      darkThemeTextColour: 'dark:text-yellow-600',
-      IconComponent: <Java className="skill-logo" />,
-      skillToolTipMsg: true,
-      imageExtraStyle: ""
+    name: 'Java Programming II',
+    certUrl:
+      'https://res.cloudinary.com/dbn5gpgi5/image/upload/v1681118770/v7jfhsjvhla0pyw1p1dr.png',
+    altName: 'Java Programming II Logo',
+    darkThemeTextColour: 'dark:text-yellow-600',
+    IconComponent: <Java className="skill-logo" />,
+    skillToolTipMsg: true,
+    imageExtraStyle: '',
   },
 ]
